9cd2ab77d8ec53f35c53abaccc924b2a462f1238
Yutao Xu <xytpai@foxmail.com>
2025-02-25 23:12:49 +0800

    Update device_property.cu

--- standard/device_property.cu
+++ standard/device_property.cu
@@ -1,6 +1,7 @@
 #include <cuda_runtime.h>
 #include <device_launch_parameters.h>
 #include <iostream>
+#include <stdexcept>
 
 #define PRINT_PROP(PARAM) std::cout << #PARAM << ": " << prop.PARAM << std::endl;
 #define ENDL_ std::cout << std::endl;
@@ -56,4 +57,6 @@ int main() {
 
     float fma_tflops = (2 * clock_freq_khz * cuda_cores) / 1e9f;
     PRINT_(fma_tflops)
+
+    throw std::runtime_error("Negative values are not allowed!"); 
 }
